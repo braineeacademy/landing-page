742ce9a6a73d22e6f12c20bb1cc9d911aeed8890
Ana Camargos <alcvlara@sga.pucminas.br>
2020-06-15 21:21:18 -0300

    updating connection with firebase

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-var firebase = require('firebase');
+// var firebase = require('firebase');
 var firebaseConfig = {
-    apiKey: "AIzaSyDvjTy2c1WASU6C00mSndF15nXuAzfmD9E",
-    authDomain: "brainee-leads.firebaseapp.com",
-    databaseURL: "https://brainee-leads.firebaseio.com",
-    projectId: "brainee-leads",
-    storageBucket: "brainee-leads.appspot.com",
-    messagingSenderId: "96149775581",
-    appId: "1:96149775581:web:dca240e24915754ee26734",
-    measurementId: "G-B3P9EDEMQ6"
-};
+        apiKey: "AIzaSyDvjTy2c1WASU6C00mSndF15nXuAzfmD9E",
+        authDomain: "brainee-leads.firebaseapp.com",
+        databaseURL: "https://brainee-leads.firebaseio.com",
+        projectId: "brainee-leads",
+        storageBucket: "brainee-leads.appspot.com",
+        messagingSenderId: "96149775581",
+        appId: "1:96149775581:web:dca240e24915754ee26734",
+        measurementId: "G-B3P9EDEMQ6"
+      };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
-
-var ref = firebase.database();
+var leadsRef = firebase.database().ref('leads');
 
 // Listen for form submit
 document.getElementById('newsletterForm').addEventListener('submit', submitForm);
@@ -31,7 +29,7 @@ function getInputVal(id) {
 
 // Send email to firebase
 function saveEmail(email) {
-    var newLeadsRef = ref.push();
+    var newLeadsRef = leadsRef.push();
     newLeadsRef.set({
         email: email
     });
