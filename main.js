1f7e5383ee401a1361f700252d9a5a7f9f1bb912
Ana Camargos <alcvlara@sga.pucminas.br>
2020-06-25 21:47:30 -0300

    Adding Sign in with google

--- main.js
+++ main.js
@@ -11,18 +11,19 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var analytics = firebase.analytics();
-var leadsRef = firebase.database().ref('leads-email');
+var leadsRef = firebase.database().ref('leads-form');
+var googleRef = firebase.database().ref('leads-google');
 
 // Listen for form submit
 document.getElementById('newsletterForm').addEventListener('submit', submitForm);
 
 function submitForm(event) {
+    firebase.analytics().logEvent('clicked_form');
     event.preventDefault();
-    firebase.analytics().logEvent('button_clicked');
 
     var email = getInputVal('email');
     var name = getInputVal('name');
-    saveEmail(email, name);
+    saveForm(email, name);
 }
 
 function getInputVal(id) {
@@ -30,7 +31,8 @@ function getInputVal(id) {
 }
 
 // Send email to firebase
-function saveEmail(email, name) {
+function saveForm(email, name) {
+    firebase.analytics().logEvent('submitted_form');
     var newLeadsRef = leadsRef.push();
     newLeadsRef.set({
         email: email,
@@ -38,7 +40,39 @@ function saveEmail(email, name) {
     });
 }
 
+function saveGoogleData(data) {
+    firebase.analytics().logEvent('submitted_google');
+    var newGoogleRef = googleRef.push();
+    newGoogleRef.set({
+        email: data.getBasicProfile().getEmail(),
+        name: data.getBasicProfile().getName()
+    });
+}
+
 function goToForm() {
     var elmnt = document.getElementById("form");
     elmnt.scrollIntoView();
-  }
+}
+
+function attachSignin(element) {
+    firebase.analytics().logEvent('clicked_google');
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+            saveGoogleData(googleUser);
+        }, function(error) {
+            alert(JSON.stringify(error, undefined, 2));
+        }
+    );
+}
+
+var googleUser = {};
+var startApp = function() {
+    gapi.load('auth2', function(){
+        auth2 = gapi.auth2.init({
+            client_id: '306712348441-hj1r4iti9ck24bduhuiekcnjg1tmc16r.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin'
+        });
+        attachSignin(document.getElementById('customGoogleBtn'));
+    });
+};
+startApp();
